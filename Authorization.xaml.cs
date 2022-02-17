4d7cfadc608bca1ce0a8cd8e610741aecb62cbbd
nizamova-2001@bk.ru <nizamova-2001@bk.ru>
2022-02-17 12:55:08 +0500

    17.02.2022

--- Authorization.xaml.cs
+++ Authorization.xaml.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System;
 using System.Web.Security;
 using System.Timers;
+using System.Windows.Threading;
 
 namespace AuthorizationOSSBSS
 {
     public partial class Authorization: Window
     {
+        string code = Membership.GeneratePassword(1, 0);//8 2 //Генерирует случайный код
         public Authorization()
         {
             InitializeComponent();
@@ -18,16 +20,16 @@ namespace AuthorizationOSSBSS
             enterButton.IsEnabled = false;       
         }
 
-        private void NumberTextPreviewTextInput (object sender, TextCompositionEventArgs e)
+        private void NumberTextPreviewTextInput (object sender, TextCompositionEventArgs e) //Запрещает ввод не числовых значений номера
         {
             e.Handled = "0123456789".IndexOf(e.Text) < 0;
         }
 
-        private void NumberTextKeyDown(object sender, KeyEventArgs e)
+        private void NumberTextKeyDown(object sender, KeyEventArgs e) //Выполняется при нажатии клавишей на текстовое поле Number
         {
             AuthorizationEntities dataContext = new AuthorizationEntities();
 
-            if (e.Key == Key.Enter)
+            if (e.Key == Key.Enter) //Определяет нажатие на клавишу Enter
             {
                 int text = Convert.ToInt32(numberText.Text);
                 int number = dataContext.Users.Where(p => p.Number == text).Select(p => p.Number).FirstOrDefault();
@@ -44,39 +46,33 @@ namespace AuthorizationOSSBSS
             }
         }
 
-        private void PasswordTextKeyDown(object sender, KeyEventArgs e)
+        private void PasswordTextKeyDown(object sender, KeyEventArgs e) //Выполняется при нажатии клавишей на текстовое поле Password
         {
             AuthorizationEntities dataContext = new AuthorizationEntities();
             Timer timer = new Timer();
 
-            if (e.Key == Key.Enter)
+            if (e.Key == Key.Enter) 
             {
                 string text = passwordText.Password;
-                string code = Membership.GeneratePassword(3, 0);//8 2
+              
                 string password = dataContext.Users.Where(p => p.Password == text).Select(p => p.Password).FirstOrDefault();
                 if (password == text)
                 {
                     if (MessageBox.Show(code, "Код", MessageBoxButton.OK, MessageBoxImage.Information) == MessageBoxResult.OK)
-                        // timer start
-                    
+                        StartTimer();
+                  
+                    codeText.IsEnabled = true;
                     enterButton.IsEnabled = true;
-
-
-
-                    if (code != codeText.Text) // || timer закончился  //после ввода сделать, сейчас раньше срабатывает
-                    {
-                        MessageBox.Show("Нажмите кнопку повторной отправки кода");
-                    }
                 }
 
                 else
                 {
                     MessageBox.Show("Неверный пароль");
-                }
+                }  
             }
         }
 
-        private void CancelButtonClick(object sender, RoutedEventArgs e)
+        private void CancelButtonClick(object sender, RoutedEventArgs e) //Очищает поля, делает кнопки не активными
         {
             numberText.Text = "";
             passwordText.Password = "";
@@ -88,20 +84,60 @@ namespace AuthorizationOSSBSS
             enterButton.IsEnabled = false;
         }
 
-        private void UpdateCodeButtonClick(object sender, RoutedEventArgs e)
+        private void UpdateCodeButtonClick(object sender, RoutedEventArgs e) 
         {
-
+            code = Membership.GeneratePassword(1, 0);//8 2
+            MessageBox.Show(code, "Код", MessageBoxButton.OK, MessageBoxImage.Information);
         }
 
         private void EnterButtonClick(object sender, RoutedEventArgs e)
         {
-            AuthorizationEntities dataContext = new AuthorizationEntities();
+            CheckCode();
+        }
 
-            int text = Convert.ToInt32(numberText.Text);
-            string role = dataContext.Persons.Where(p => p.User.Number == text).Select(p => p.Role.Role1).FirstOrDefault();
-            MessageBox.Show("Добро пожаловать " + role);
+        private void TimerTick(object sender, EventArgs e) //Происходит по истечении интервала времени
+        {
+            DispatcherTimer timer = (DispatcherTimer)sender;
+            timer.Stop();
+            timer.Tick -= TimerTick;
+
+            CheckCode();
+        }
+
+        private void StartTimer() 
+        {
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(10);
+            timer.Tick += TimerTick;
+            timer.Start();
+        }
+
+        private void CheckCode() 
+        {
+            
+            if (codeText.Text != code)
+            {
+                MessageBox.Show("Нажмите кнопку повторной отправки кода");
+                updateCodeButton.IsEnabled = true;
+               
+                code = null;
+            }
+            else
+            {
+                AuthorizationEntities dataContext = new AuthorizationEntities();
+
+                int text = Convert.ToInt32(numberText.Text);
+                string role = dataContext.Persons.Where(p => p.User.Number == text).Select(p => p.Role.Role1).FirstOrDefault();
+                MessageBox.Show("\tДобро пожаловать \n" + role);
+            }
+        }
+
+        private void CodeTextKeyDown(object sender, KeyEventArgs e) //Выполняется при нажатии клавишей на текстовое поле Code
+        {
+            if (e.Key == Key.Enter)
+            {
+                CheckCode();
+            }
         }
     }
 }
-
-
